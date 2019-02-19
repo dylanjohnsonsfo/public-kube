be757eb90d8295a53f7214379227850e38c99a49
dylan <djohnson@jawbone.com>
2019-02-19 09:12:20 -0700

    update curl

--- istio/curl.sh
+++ istio/curl.sh
@@ -1,11 +1,2 @@
-for i in `seq 1 2000000`; do curl http://datashare.services.trimblevisibility.tech/api/diagnostics/alive; > /dev/null 2>&1; done \
- 
-& for i in `seq 1 2000000`; do curl http://myapp.services.trimblevisibility.tech; > /dev/null 2>&1; done \
-
-& for i in `seq 1 2000000`; do curl https://carrierback.services.trimblevisibility.tech/api/diagnostics/alive; > /dev/null 2>&1; done \
-
-& for i in `seq 1 2000000`; do curl https://myapp.services.trimblevisibility.tech; > /dev/null 2>&1; done
-
-
-#& for i in `seq 1 2000000`; do curl http://datashare.services.trimblevisibility.tech/api/diagnostics/failed > /dev/null 2>&1; done 
+for i in `seq 1 2000000`; do curl http://datashare.services.trimblevisibility.tech/api/diagnostics/alive; > /dev/null 2>&1; done & for i in `seq 1 2000000`; do curl http://myapp.services.trimblevisibility.tech; > /dev/null 2>&1; done & for i in `seq 1 2000000`; do curl https://carrierback.services.trimblevisibility.tech/api/diagnostics/alive; > /dev/null 2>&1; done & for i in `seq 1 2000000`; do curl https://myapp.services.trimblevisibility.tech; > /dev/null 2>&1; done
 
